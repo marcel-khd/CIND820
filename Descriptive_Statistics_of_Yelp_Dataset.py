5e852285c6782024eb0ee8d16d340c71c30f414f
Marcel K <52582235+marcel-khd@users.noreply.github.com>
2022-11-21 21:25:51 -0500

    Update Descriptive_Statistics_of_Yelp_Dataset.py
    Adding new descriptive statistics about reviews

--- Descriptive_Statistics_of_Yelp_Dataset.py
+++ Descriptive_Statistics_of_Yelp_Dataset.py
@@ -11,7 +11,7 @@ import seaborn as sns
 get_ipython().run_line_magic('matplotlib', 'inline')
 
 
-# In[2]:
+# In[6]:
 
 
 #Read business object and display it's columns
@@ -75,16 +75,6 @@ df_by_state
 df_categories = business_df[['categories','business_id']].groupby(['categories']).count()
 df_categories.sort_values(by=['business_id'],ascending=False, inplace=True)
 df_categories[df_categories['business_id']>400]
-
-
-# In[10]:
-
-
-#Find number of restaurants
-restaurant_df = business_df[business_df['categories'].str.contains('Restaurant',case=False, na=False)]
-restaurant_df = restaurant_df[['categories','business_id']].groupby(['categories']).count()
-restaurant_df.sort_values(by=['business_id'],ascending=False, inplace=True)
-restaurant_df.count()
 
 
 # In[ ]:
@@ -196,3 +186,109 @@ print('There are  ' + str(count_row) + ' reviews on or after 2018-01-01')
 
 columns = reviews_df_2018.head()
 columns 
+
+
+# In[3]:
+
+
+#Read a selected city reviews from csv file 
+#Prerequisite: run Recommender.ipynb
+reviews_selected_Restaurants = pd.read_csv('reviews_selected_city_restaurants_Clean.csv')
+
+
+# In[4]:
+
+
+reviews_selected_Restaurants.head()
+
+
+# In[5]:
+
+
+#Nuber of reviews by business
+reviews_by_business = reviews_selected_Restaurants[['business_id','review_id']].groupby(['business_id']).count()
+reviews_by_business.sort_values(by=['review_id'],ascending=False, inplace=True)
+reviews_by_business = reviews_by_business.rename(columns={'review_id': 'nbr_of_reviews'})
+reviews_by_business[reviews_by_business['nbr_of_reviews']>2000]
+
+
+# In[6]:
+
+
+#Frequency of reviews
+#reviews_by_business['range'] = pd.cut(reviews_by_business.nbr_of_reviews, bins=[0,11,101,1001,10001], labels=["0-10","11-100","101-1000","1001+"])
+reviews_by_business['range'] = pd.cut(reviews_by_business.nbr_of_reviews, bins=[0,51,101,151,10000], labels=["0-50","51-100","101-150","151+"])
+#reviews_by_business['range'] = pd.cut(reviews_by_business.nbr_of_reviews,50)
+sns.countplot(x='range',data=reviews_by_business)
+
+
+# In[7]:
+
+
+#Frequency of reviews
+reviews_by_business_grouped = reviews_by_business[['range','nbr_of_reviews']].groupby(['range']).count()
+reviews_by_business_grouped = reviews_by_business_grouped.rename(columns={'nbr_of_reviews': 'nbr_of_business'})
+#reviews_by_business_grouped.sort_values(by=['nbr_of_reviews'],ascending=False, inplace=True)
+reviews_by_business_grouped
+
+
+# In[39]:
+
+
+#Desciptive statistics about number of reviews by business
+reviews_by_business['nbr_of_reviews'].describe()
+
+
+# In[8]:
+
+
+#Note: Number of businesses decreases exponentially with number of reviews
+hist = reviews_by_business[reviews_by_business['nbr_of_reviews']<600].hist(bins=61)
+
+
+# In[9]:
+
+
+hist = reviews_by_business[reviews_by_business['nbr_of_reviews'] <=50].hist()
+
+
+# In[10]:
+
+
+#Nuber of reviews by year
+reviews_selected_Restaurants['year'] = reviews_selected_Restaurants['date'].str[:4]
+reviews_by_year = reviews_selected_Restaurants[['year','review_id']].groupby(['year']).count()
+reviews_by_year = reviews_by_year.rename(columns={'review_id': 'nbr_of_reviews'})
+reviews_by_year
+
+
+# In[11]:
+
+
+#Number of reviews by year: 2018-2019 have highest number of reviews
+#There is a drop in 2020 due to Covid
+reviews_by_year = reviews_by_year.reset_index()
+sns.barplot(data=reviews_by_year,x='year',y='nbr_of_reviews')
+
+
+# In[27]:
+
+
+#Find number of characters in a review
+def count_text(string_input):
+    string_input = str(string_input)
+    nbr_chars = len(string_input)
+    return nbr_chars
+
+
+reviews_selected_Restaurants['nbr_characters'] = reviews_selected_Restaurants['text'].apply(count_text)
+hist = reviews_selected_Restaurants['nbr_characters'].hist()
+
+
+# In[38]:
+
+
+#Desciptive statistics about number of characters
+#Note that minimum number of charactes is 3, with mean = 360
+#Number of characters in 1st quartile = 149
+reviews_selected_Restaurants['nbr_characters'].describe()
